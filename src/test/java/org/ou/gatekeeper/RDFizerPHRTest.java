1f8f5fe95fa8582f5e0b15f622117f2876a9a853
Riccardo Pala <riccardo.pietro.pala@gmail.com>
2022-05-23 12:19:28 +0200

    Updated test digests for new identifier rules

--- src/test/java/org/ou/gatekeeper/RDFizerPHRTest.java
+++ src/test/java/org/ou/gatekeeper/RDFizerPHRTest.java
@@ -1,6 +1,7 @@
 package org.ou.gatekeeper;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.commons.ResourceUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.ou.gatekeeper.fhir.adapters.FHIRAdapter;
@@ -24,17 +25,17 @@ class RDFizerPHRTest {
   @ParameterizedTest
   @CsvSource({
     // Patient
-//    "da67f2fd35e4bb3533fa7c95a6fd7bfe9a5d0ac945e0fe77a4e1d9c90c597f84, '000,001,002',         datasets/saxony/phr/dataset-Patient.json",
+    "da67f2fd35e4bb3533fa7c95a6fd7bfe9a5d0ac945e0fe77a4e1d9c90c597f84, '000,001,002',         datasets/saxony/phr/dataset-Patient.json",
     // Observations
 //    "xxx, '000,001,002,006',     datasets/saxony/phr/01-dataset-complete.json", // ObservationTimeSpan
-//    "039715a93d34f1ce55d9f3cc69a04aeda942e6d65075d54585fab1473be85a90, '000,001,002,003,006,013', datasets/saxony/phr/dataset-BodyHeight.json",
-    "xxx, '000,001,002,003,006,040', datasets/saxony/phr/dataset-BloodPressure.json",
-//    "xxx, '000,001,002,003,006,040', datasets/saxony/phr/dataset-BloodPressure-multiple.json",
-//    "xxx, '000,001,002,003,006,060', datasets/saxony/phr/dataset-Cardiovascular.json",
-//    "xxx, '000,001,002,003,006,060', datasets/saxony/phr/dataset-Cardiovascular-multiple.json",
-//    "xxx, '000,001,002,003,006,070', datasets/saxony/phr/dataset-Respiration.json",
-//    "xxx, '000,001,002,003,006,101', datasets/saxony/phr/dataset-Sleep.json",
-//    "xxx, '000,001,002,003,006,102', datasets/saxony/phr/dataset-Walking.json"
+    "039715a93d34f1ce55d9f3cc69a04aeda942e6d65075d54585fab1473be85a90, '000,001,002,003,006,013', datasets/saxony/phr/dataset-BodyHeight.json",
+    "45ef4b2a7f36109d1aaac43bdc00f3c845bba64327a1dd895d36b33686367cb5, '000,001,002,003,006,040', datasets/saxony/phr/dataset-BloodPressure.json",
+    "b1945ca173e328938bc6f35d3416214bb9a4d56be63216cb52a232cbe2f08f86, '000,001,002,003,006,040', datasets/saxony/phr/dataset-BloodPressure-multiple.json",
+    "ea997ea8e397232630e5d6ea4641f310e779661a267a356caf2862e867195d5c, '000,001,002,003,006,060', datasets/saxony/phr/dataset-Cardiovascular.json",
+    "56b6d15a2ae0194cee3bbb34677ec6480fa4c36fe343fc21f4f1dee2d8925a3c, '000,001,002,003,006,060', datasets/saxony/phr/dataset-Cardiovascular-multiple.json",
+    "33bd4b2db107bb8f58c84aa966be152fd724f4ea012ed319de08332765014e69, '000,001,002,003,006,070', datasets/saxony/phr/dataset-Respiration.json",
+    "59bc9411703d40e591ad243e4bb1bb45e64e02978d8cb5164df996dd1a292796, '000,001,002,003,006,101', datasets/saxony/phr/dataset-Sleep.json",
+    "37913fba703414073d192c23218dd8eca6b686b86d6e4858216f2db8e2874633, '000,001,002,003,006,102', datasets/saxony/phr/dataset-Walking.json"
   })
   void test_transform_FHIRtoRDF(String expectedDigest, String modules, String dataset) {
     File datasetFile = TestUtils.loadResource(dataset);
@@ -55,7 +56,7 @@ class RDFizerPHRTest {
       assertEquals(expectedDigest, outputDigest);
     } catch (IOException e) {
     } finally {
-//      ResourceUtils.clean(outputFile); // @todo uncomment this when tests completed
+      ResourceUtils.clean(outputFile);
     }
   }
 
