4f7a4d28b16216272048e6c18a4c83c1a029450d
Riccardo Pala <riccardo.pietro.pala@gmail.com>
2022-10-11 18:37:36 +0200

    Removed deprecated method

--- src/main/java/org/ou/gatekeeper/RDFizer.java
+++ src/main/java/org/ou/gatekeeper/RDFizer.java
@@ -1,14 +1,12 @@
 package org.ou.gatekeeper;
 
 import org.apache.commons.io.FileUtils;
-import org.commons.FilenameUtils;
 import org.commons.ResourceUtils;
 import org.ou.gatekeeper.fhir.adapters.FHIRAdapter;
 import org.ou.gatekeeper.rdf.RDFMapper;
 import org.ou.gatekeeper.rdf.mappings.RMLMapping;
 
 import java.io.File;
-import java.util.Iterator;
 
 import static org.commons.ResourceUtils.generateUniqueFilename;
 
@@ -17,32 +15,6 @@ import static org.commons.ResourceUtils.generateUniqueFilename;
  * @todo description
  */
 public class RDFizer {
-
-  /**
-   * Reads datasets from a folder and writes the result in there.
-   * @param datasets list of file to convert
-   * @param converter the adapter that understands the input dataset
-   * @param mapping RML mapping file which contains mapping rules
-   * @param outputFolder folder where save the RDF output files
-   * @param newExtension extension of output files {@link org.ou.gatekeeper.rdf.enums.OutputFormat}
-   * */
-  @Deprecated
-  public static void trasform(
-    Iterator<File> datasets,
-    FHIRAdapter converter,
-    RMLMapping mapping,
-    File outputFolder,
-    String newExtension // @todo workround
-  ) {
-    while (datasets.hasNext()) {
-      File dataset = datasets.next();
-      String trimmedDatasetName = FilenameUtils.trim2LvlExtension(dataset.getName());
-      String outputFilename = "output-" + FilenameUtils
-        .changeExtension(trimmedDatasetName, newExtension);
-      File output = new File(outputFolder, outputFilename);
-      trasform(dataset, converter, mapping, output);
-    }
-  }
 
   /**
    * Reads dataset from file and writes the result on a file.
